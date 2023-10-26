deb1f3b5dc60128f3d8bae7ae7ec6ee4d4a09807
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-26 11:01:59 +0700

    no1

--- KodeB.java
+++ KodeB.java
@@ -6,6 +6,7 @@ public class KodeB{
         double tesAkademik, tesBing, tesNasionalis;
         double rataRata = 0;
 
+        //Nilai Rata-Rata 
         System.out.println("Masukkan Nilai Tes Potensi Akademik : ");
         tesAkademik = input03.nextDouble();
         System.out.println("Masukkan Nilai Tes Bahasa Inggris : ");
@@ -14,6 +15,8 @@ public class KodeB{
         tesNasionalis = input03.nextDouble();
 
         rataRata = (tesAkademik + tesBing + tesNasionalis)/3;
-        System.out.println("Nilai rata-rata calon mahasiswa adalah : " + rataRata);
+        System.out.println("Nilai rata-rata calon karyawan adalah : " + rataRata);
+
+
     }
 }
